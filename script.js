320508017625094bfe0b3f968d772fd64e07165e
ianiglesias-developer <149393170+ianiglesias-developer@users.noreply.github.com>
2023-11-18 00:10:00 +0000

    sumulating console.log(js exercices)

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Name your player");
+
+let nickname = "Charles"
+
+console.log("Welcome" + nickname);
+
+//-------------------------------------------
+
+const notify = "Pokémon GO says:"
+
+console.log(notify + "Here is a new Pokémon!");
+console.log(notify + "This is your enemy!");
