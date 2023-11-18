0d6c9686d472a80b00f0c000d750b16ac9f91933
ianiglesias-developer <149393170+ianiglesias-developer@users.noreply.github.com>
2023-11-18 13:54:01 +0000

    i don´t know...

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+console.log("Digite o nome do seu jogador");
+
 console.log("Name your player");
 
 let nickname = "Charles"
@@ -6,7 +8,7 @@ console.log("Welcome" + nickname);
 
 //-------------------------------------------
 
-const notify = "Pokémon GO says:"
+const notify = "Pokemon GO says:"
 
-console.log(notify + "Here is a new Pokémon!");
+console.log(notify + "Here is a new Pokemon!");
 console.log(notify + "This is your enemy!");
