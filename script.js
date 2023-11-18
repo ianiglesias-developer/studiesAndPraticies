cd7ddff3087bc922d9d17bceedc23d47d562f964
ianiglesias-developer <149393170+ianiglesias-developer@users.noreply.github.com>
2023-11-18 14:22:36 +0000

    name types of variables

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-console.log("Digite o nome do seu jogador");
-
-console.log("Name your player");
+console.log("Name your player:");
 
 let nickname = "Charles"
 
-console.log("Welcome" + nickname);
+console.log("Welcome " + nickname);
 
 //-------------------------------------------
 
-const notify = "Pokemon GO says:"
+const notify = "Pokemon GO says: "
 
 console.log(notify + "Here is a new Pokemon!");
 console.log(notify + "This is your enemy!");
+
+// Pokédex
+
+let namePokemon = "Pikachu"        // String são variáveis que armazenam texto
+let levelPokemon = 20              // Number são variáveis que armazenam números
+let lifePokemon = 45
+let genderPokemon = "Male"        
+let alivePokemon = true            // Boolean (ou Booleanos(as)) são variáveis LÓGICAS que armazenam apenas "true" ou "false"
+
+//-------------------------------------------
+
