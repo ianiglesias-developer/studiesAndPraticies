817320ae8f3b42baf2f314cd0765e1f19e988a69
iansantos4352 <149393170+iansantos4352@users.noreply.github.com>
2023-11-27 21:00:16 +0000

    incemento e decremento

--- arithmeticOperators/script.js
+++ arithmeticOperators/script.js
@@ -1,5 +1,24 @@
 let idade = 30;
 let anoAtual = 2023
 
-// A expressão a seguir definirá em que ano a pessoa nasceu
+// A expressão a seguir definirá em que ano a pessoa nasceu:
+
 console.log("O usuário naceu em " + (anoAtual - idade));
+
+// Incremento de Decremento:
+
+// O ++ adiciona +1 e o -- subtrai -1
+
+idade++
+console.log(idade);
+
+idade++
+idade++
+console.log(idade);
+
+// Calcular e Atribuir:
+
+let idadeJoao = 10;
+idadeJoao += 5; // idadeJoao = idadeJoao + 5
+
+console.log(idadeJoao);
